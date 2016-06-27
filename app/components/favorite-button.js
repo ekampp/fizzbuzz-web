86e7771b2bb1383c5afe155d9b79a9e3b443f4cd
Emil Kampp <emil@kampp.me>
2016-06-27 21:23:34 +0200

    Consume the fizz buzz api

--- app/components/favorite-button.js
+++ app/components/favorite-button.js
@@ -1,15 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service('store'),
+
   tagName: 'span',
 
   actions: {
     favorite(number) {
       console.log('favoring', number);
+      this.get('store').createRecord('favorite', {
+        number: number,
+      }).save();
+      this.get('store').findRecord('number', number).then((number) => {
+        number.set('favorite', true);
+      })
     },
 
     unfavorite(number) {
       console.log('unfavoring', number);
+      this.get('store').findRecord('favorite', number).then((fav) => {
+        if(fav.destroyRecord()) {
+          this.get('store').findRecord('number', number).then((number) => {
+            number.set('favorite', false);
+          });
+        }
+      });
     },
   }
 });
